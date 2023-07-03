f89b514b699936926250b4352e528d4373ae2d3e
elmurodWeb <elmurodorinboyev@gmail.com>
2023-07-03 20:37:29 +0500

    Solve some problems

--- js/google_map.js
+++ js/google_map.js
@@ -2,12 +2,8 @@
 var google;
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
+    
     var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
